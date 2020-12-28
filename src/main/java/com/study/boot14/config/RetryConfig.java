fd717edb4468356e8408475a5c0a9c310d86aeb4
inee7 <jongin012@gmail.com>
2020-12-28 20:22:28 +0900

    spring retry

---
+++ src/main/java/com/study/boot14/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.study.boot14.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
